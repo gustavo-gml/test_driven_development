09948c38a183f88944aa4acda6f4033983aa5e50
gustavo-gml <gustavomartinslima06@gmail.com>
2026-06-09 21:49:34 -0300

    adiciona testes de email e cpf duplicado e crição com sucesso

--- src/tests/UsuarioRepositoryTest.java
+++ src/tests/UsuarioRepositoryTest.java
@@ -1,13 +1,11 @@
 package tests;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.*;
 
 import main.models.Usuario;
 import main.repository.UsuarioRepository;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 
 import java.time.LocalDate;
 
@@ -27,9 +25,39 @@ public class UsuarioRepositoryTest {
 
         Usuario usuarioSalvo = repository.salvar(novoUsuario);
 
-        assertNotNull(usuarioSalvo, "O objeto não poderia estar vazio.");
+        assertNotNull(usuarioSalvo, "O objeto salvo não deveria ser nulo.");
+        assertNotNull(usuarioSalvo.getId(), "O id deveria ter sido gerado.");
+    }
 
-        assertNotNull(usuarioSalvo.getId(), "O id não foi inserido corretamente.");
+    @Test
+    void criarEmailDuplicado() {
+        Usuario novoUsuario = new Usuario("Matheus", "matheusgedesinho@gmail.com", "122.322.323-15",
+                LocalDate.parse("2026-06-05"));
+
+        Usuario novoUsuario2 = new Usuario("Mathias", "matheusgedesinho@gmail.com", "122.322.323-15",
+                LocalDate.parse("2026-06-05"));
+
+        repository.salvar(novoUsuario);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            repository.salvar(novoUsuario2);
+        }, "Nao deveria ser possivel salvar um usuario com o mesmo email");
+
+    }
+
+    @Test
+    void criarCpfDuplicado() {
+        Usuario novoUsuario = new Usuario("Matheus", "matheusgedesinho@gmail.com", "122.322.323-15",
+                LocalDate.parse("2026-06-05"));
+
+        Usuario novoUsuario2 = new Usuario("Mathias", "matheusgedesinho@gmail.com", "122.322.323-15",
+                LocalDate.parse("2026-06-05"));
+
+        repository.salvar(novoUsuario);
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            repository.salvar(novoUsuario2);
+        }, "Nao deveria ser possivel salvar um usuario com o mesmo cpf");
 
     }
 
